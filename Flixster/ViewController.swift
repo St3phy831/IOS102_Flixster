4ad7bac0f751cefe95bea6082d324db74c834e08
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-09 12:15:44 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Stephanie Hernandez on 2/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
