c3db519cda6fbdd4726e99945175a20f5bc9c0e0
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-09 14:07:12 -0800

    Setup table view

--- Flixster/ViewController.swift
+++ Flixster/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    var movies: [Movie] = []
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        
+        movies = Movie.mockMovies
+        print(movies)
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell with identifier, "MovieCell"
+        // the `dequeueReusableCell(withIdentifier:)` method just returns a generic UITableViewCell so it's necessary to cast it to our specific custom cell.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
 
+        // Get the track that corresponds to the table view row
+        let movie = movies[indexPath.row]
+
+        // Configure the cell with it's associated track
+        cell.configure(with: movie)
+
+        // return the cell for display in the table view
+        return cell
+    }
 }
 
