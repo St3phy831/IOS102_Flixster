7190cf498454e7194f76e5c757d283e29ce5cd8c
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-16 14:52:59 -0800

    Updated collection view layout

--- Flixster/PostersViewController.swift
+++ Flixster/PostersViewController.swift
@@ -64,6 +64,28 @@ class PostersViewController: UIViewController, UICollectionViewDataSource {
         task.resume()
         
         collectionView.dataSource = self
+        
+        // Get a reference to the collection view's layout
+        // We want to dynamically size the cells for the available space and desired number of columns.
+        // NOTE: This collection view scrolls vertically, but collection views can alternatively scroll horizontally.
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+        // The minimum spacing between adjacent cells (left / right, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumInteritemSpacing = 1
+
+        // The minimum spacing between adjacent cells (top / bottom, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumLineSpacing = 1
+
+        // Set this to however many columns you want to show in the collection.
+        let numberOfColumns: CGFloat = 2
+
+        // Calculate the width each cell need to be to fit the number of columns, taking into account the spacing between cells.
+        let width = (collectionView.bounds.width - layout.minimumInteritemSpacing * (numberOfColumns - 1)) / numberOfColumns
+
+        // Set the size that each tem/cell should display at
+        layout.itemSize = CGSize(width: width, height: width*1.5)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
