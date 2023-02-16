2ab5be8466afd17af57bfacbc806ecf2e5b35685
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-16 14:36:21 -0800

    Added collection view and setup cell

---
+++ Flixster/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flixster
+//
+//  Created by Stephanie Hernandez on 2/16/23.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+}
