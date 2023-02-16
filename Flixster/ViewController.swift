87fb59c9b228e31a55c1a5a527cba7ad34987094
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-16 12:37:34 -0800

    Able to fetch data

--- Flixster/ViewController.swift
+++ Flixster/ViewController.swift
@@ -15,10 +15,44 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Create a URL for the request
+        // In this case, the custom search URL you created in in part 1
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=0204673f3765071240a29f1abe020da1")!
+
+        // Use the URL to instantiate a request
+        let request = URLRequest(url: url)
+
+        // Create a URLSession using a shared instance and call its dataTask method
+        // The data task method attempts to retrieve the contents of a URL based on the specified URL.
+        // When finished, it calls it's completion handler (closure) passing in optional values for data (the data we want to fetch), response (info about the response like status code) and error (if the request was unsuccessful)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+
+            // Handle any errors
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
+
+            // Make sure we have data
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+
+            // The `JSONSerialization.jsonObject(with: data)` method is a "throwing" function (meaning it can throw an error) so we wrap it in a `do` `catch`
+            // We cast the resultant returned object to a dictionary with a `String` key, `Any` value pair.
+            do {
+                let jsonDictionary = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                print(jsonDictionary)
+            } catch {
+                print("❌ Error parsing JSON: \(error.localizedDescription)")
+            }
+        }
+
+        // Initiate the network request
+        task.resume()
+        
         tableView.dataSource = self
         
-        movies = Movie.mockMovies
-        print(movies)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
